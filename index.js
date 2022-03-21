962170a96db3ca7d215df0f7f2ee0a85f803cb11
Jasmin <ssongnari@gmail.com>
2022-03-21 09:07:35 -0400

    commit tree d2f27a802e11c2f4a0afc34c8477a2b8d93e3a76

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code goes here
+// document.addEventListener("DOMContentLoaded", function () {
+//     // let text = document.getElementById('text').textContent = `${text}`;
+//     // console.log(text);
+//     // document.querySelector('p');
+//     let text = document.getElementById('text').textContent;
+//     document.addEventListener('DOMContentLoaded', () => {
+//         console.log(`${text}`);
+//     })
+// });
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('text').textContent = 'This is really cool!'
+})
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     document.getElementById('text').textContent = "This is really cool!"
+// });
